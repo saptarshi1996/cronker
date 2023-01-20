835851c7ee022b03fe021435d98bfbcba0fada5c
saptarshi1996 <saptarshisg96@gmail.com>
2023-01-21 03:39:39 +0530

    py server

--- client/components/cron/CronDetailsModal.tsx
+++
@@ -1,5 +0,0 @@
-export default function CronDetailsModal() {
-  return (
-    <></>
-  )
-}
--- src/interfaces/config/environment.ts
+++
@@ -1,7 +0,0 @@
-export default interface IEnvironment {
-  HOST: string
-  API_PORT: number
-  WORKER_PORT: number
-  REDIS_HOST: string
-  REDIS_PORT: number
-}
